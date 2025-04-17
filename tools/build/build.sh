15f38abafc05b04617a79e82d767289581c762ef
Adam <38027065+adamspaini@users.noreply.github.com>
2025-04-17 03:20:04 -0400

    Update build.sh

--- tools/build/build.sh
+++ tools/build/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Thanks to clhex for the script (Github username: clhexftw)
-
+# Configuración inicial
 kernel_dir="${PWD}"
 CCACHE=$(command -v ccache)
 objdir="${kernel_dir}/out"
@@ -20,26 +19,26 @@ export KBUILD_BUILD_USER=arch-linux
 
 export PATH="$CLANG_DIR/bin:$GCC64_DIR/bin:$GCC32_DIR/bin:$PATH"
 
-# Crdroid Clang
-if ! [ -d "$CLANG_DIR" ]; then
-    echo "Clonando Crdroid Clang..."
-    if ! git clone -q --depth=1 --single-branch https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git -b 15.0 $CLANG_DIR; then
-        echo "¡Fallo al clonar Crdroid Clang!"
-        exit 1
+# Clonar herramientas
+clone_tools() {
+    # Crdroid Clang
+    if ! [ -d "$CLANG_DIR" ]; then
+        echo "Clonando Crdroid Clang..."
+        git clone -q --depth=1 --single-branch https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r547379.git -b 15.0 $CLANG_DIR
     fi
-fi
 
-# GCC64
-if ! [ -d "$GCC64_DIR" ]; then
-    echo "Clonando GCC64..."
-    git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git $GCC64_DIR
-fi
+    # GCC64
+    if ! [ -d "$GCC64_DIR" ]; then
+        echo "Clonando GCC64..."
+        git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git $GCC64_DIR
+    fi
 
-# GCC32
-if ! [ -d "$GCC32_DIR" ]; then
-    echo "Clonando GCC32..."
-    git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git $GCC32_DIR
-fi
+    # GCC32
+    if ! [ -d "$GCC32_DIR" ]; then
+        echo "Clonando GCC32..."
+        git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git $GCC32_DIR
+    fi
+}
 
 # Colores
 NC='\033[0m'
@@ -49,7 +48,7 @@ LGR='\033[1;32m'
 
 make_defconfig()
 {
-    START=$(date +"%")
+    START=$(date +"%s")
     echo -e ${LGR} "########### Generando Defconfig ############${NC}"
     make -s ARCH=${ARCH} O=${objdir} ${CONFIG_FILE} -j$(nproc) -l$(nproc)
 }
@@ -62,26 +61,25 @@ compile()
     O=out \
     ARCH=arm64 \
     CC="ccache clang" \
-    SUBARCH=arm64 \  
-    DTC_EXT=dtc \  
-    CLANG_TRIPLE=aarch64-linux-gnu- \ 
+    SUBARCH=arm64 \
+    DTC_EXT=dtc \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
     CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
     AR=llvm-ar \
-    STRIP=llvm-strip \  
-    OBJCOPY=llvm-objcopy \  
-    OBJDUMP=llvm-objdump \  
-    HOSTCC=clang \  
-    HOSTCXX=clang++ \  
-    HOSTAR=llvm-ar \  
-    HOSTLD=ld.lld \     
+    STRIP=llvm-strip \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    HOSTCC=clang \
+    HOSTCXX=clang++ \
+    HOSTAR=llvm-ar \
+    HOSTLD=ld.lld \
     LLVM_NM=llvm-nm \
-    LD=ld.lld 
+    LD=ld.lld \
     NM=llvm-nm \
     LLVM=1 \
     LLVM_IAS=1
-    $1
 }
 
 completion()
@@ -89,7 +87,7 @@ completion()
     cd ${objdir}
     COMPILED_IMAGE=arch/arm64/boot/Image
     COMPILED_DTBO=arch/arm64/boot/dtbo.img
-    if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
+    if [[ -f ${COMPILED_IMAGE} ]]; then
         git clone -q https://github.com/GXC2356/AnyKernel3.git -b master $anykernel
         mv -f $ZIMAGE ${COMPILED_DTBO} $anykernel
         cd $anykernel
@@ -100,11 +98,13 @@ completion()
         echo -e ${LGR} "#### build completed successfully ####"
         exit 0
     else
-        echo -e ${LGR} "#### failed to build some targets ####"
+        echo -e ${RED} "#### failed to build some targets ####"
+        exit 1
     fi
 }
 
+# Ejecución principal
+clone_tools
 make_defconfig
 compile | tee out/log.txt
 completion
-cd ${kernel_dir}
